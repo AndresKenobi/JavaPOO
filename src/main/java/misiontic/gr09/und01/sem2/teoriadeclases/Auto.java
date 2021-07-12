6098ad4bd37e321a6d24ffdac751231649f9edf3
andre <andre@LAPTOP-J768PEML>
2021-07-12 17:01:56 -0500

    prog orie obj Actualizacion tema de herencia

---
+++ src/main/java/misiontic/gr09/und01/sem2/teoriadeclases/Auto.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package misiontic.gr09.und01.sem2.teoriadeclases;
+
+/**
+ *
+ * @author andre
+ */
+class Auto extends Vehiculo {
+    
+    protected String modelo_Tipo = "Mustang";    // Car attribute
+    
+}
