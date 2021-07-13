b5809fced8f102132df1dea0e3a5dce72da370e5
andre <andre@LAPTOP-J768PEML>
2021-07-13 18:32:34 -0500

    prog orie obj pruebas Clase automovil primeros objetos Clase Vehiculo Getters and Setters Clase Auto tema de herencia/polimorfismo Clase externa tema de Clases anidadas Clase barco tema de polimorfismo

--- src/main/java/misiontic/gr09/und01/sem2/teoriadeclases/Auto.java
+++ src/main/java/misiontic/gr09/und01/sem2/teoriadeclases/Auto.java
@@ -12,5 +12,9 @@ package misiontic.gr09.und01.sem2.teoriadeclases;
 class Auto extends Vehiculo {
     
     protected String modelo_Tipo = "Mustang";    // Car attribute
-    
+//We set the brand attribute in Vehicle to a protected access modifier. 
+//If it was set to private, the Vehiculo class would not be able to access it.
+    public void honk() {                            // Vehicle method
+    System.out.println("Peep, peep!");
+    }
 }
