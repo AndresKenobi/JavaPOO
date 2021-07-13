c4b3dc2a5aa7d45a046b449042b7da510097bae4
andre <andre@LAPTOP-J768PEML>
2021-07-13 18:29:47 -0500

    prog orie obj pruebas Actualizacion tema de herencia Clase externa tema de Clases anidadas Clase barco tema de polimorfismo

--- src/main/java/misiontic/gr09/und01/sem2/teoriadeclases/Prueba.java
+++ src/main/java/misiontic/gr09/und01/sem2/teoriadeclases/Prueba.java
@@ -6,7 +6,8 @@
 package misiontic.gr09.und01.sem2.teoriadeclases;
 
 //import static misiontic.gr09.und01.sem2.teoriadeclases.MiprimeraClase.impresion;
-
+import java.util.Date;
+import java.time.*;
 /**
  *
  * @author andre
@@ -21,7 +22,7 @@ class Prueba {
         //Crear un objeto
         //Nombre de la Clase  nombre del objeto = new  nombre del la clase();
         //Un objeto es una instancia de una clase. Un objeto hereda los atributos y metodos de la clase.
-        
+        /*
         MiprimeraClase objUno = new MiprimeraClase();  //objeto 1 
         MiprimeraClase objDos = new MiprimeraClase(); //objeto 2
         
@@ -43,7 +44,9 @@ class Prueba {
         objUno.verificar3(objUno.y); // metodo public  Aparece en las sugerencias de notacion de punto
         //acepta el atributo y como argumento. 
         System.out.println("<<<<<<<<---------------------------------------------------->>>>>>>>>\n " );
+        */
         
+        /*
         //Prueba del constructor
         
         Automovil autoN = new Automovil( 1420, "Generico"); //puede tener parametros
@@ -53,9 +56,67 @@ class Prueba {
         System.out.println("Pueden ir en él : " + autoN.numPer + "personas");
         System.out.println("El auto es del tipo : " + autoN.tipo);
         System.out.println("La velocidad que alcanza es : " + autoN.vel + " km/seg");
+        */
         
+        /*
+        Year año =   Year.now() ;  //Tipo de dato Year????
+        System.out.println("El año actual es : " + año);
+        //Pero no puedo hacer operaciones de suma o resta con el porque no es un entero
+        //solucion: int modelo = Year.now().getValue();
+        */
         
+        /*
+        System.out.println("<<<<<Herencia>>>>>\n" );
+        //
+        Auto auto_1 = new Auto(); //Creamos una instancia u objeto auto.
         
+        auto_1.setModelo(2020); //Setter de la clase vehiculo
+        System.out.println("Marca: " + auto_1.marca + "\n" ); //herede el atributo de vehiculo
+        System.out.println("Modelo (año): " + auto_1.getModelo() + "\n" ); //Getter de la clase vehiculo
+        System.out.println("Prototipo : " + auto_1.marca + " " + auto_1.modelo_Tipo + " " + auto_1.getModelo() + "\n" );
+        auto_1.honk();
+       */
+        
+        /*
+        //Polimorfismo - De la mano con la herencia
+        Like we specified in the previous chapter; Inheritance lets us inherit attributes and methods
+        from another class. Polymorphism uses those methods to perform different tasks. 
+        This allows us to perform a single action in different ways.
+
+        For example, think of a superclass called Animal that has a method called animalSound().
+        Subclasses of Animals could be Pigs, Cats, Dogs, Birds - And they also have their own 
+        implementation of an animal sound (the pig oinks, and the cat meows, etc.):
+        */
+        
+         /*
+        System.out.println("<<<<<Polimorfismo>>>>>\n" );
+        //Creamos los objetos de una manera diferente....
+        Vehiculo v_Auto = new Auto();  //Vehiculo - Auto
+        Vehiculo v_Barco = new Barco(); //Vehiculo - Barco
+        
+        System.out.println("<<<<< toca la bocina >>>>>\n" );
+        System.out.println("Auto.....");
+        v_Auto.honk();
+        System.out.println("Barco....." );
+        v_Barco.honk(); // La misma funcion/metodo para multiples objetos
+        */
+        
+        System.out.println("<<<<<Clases anidadas o internas>>>>>\n" );
+        /*
+        Externa c_externa = new Externa(); // creamos un objeto de la clase externa
+        Externa.Interna c_interna = c_externa.new Interna(); //con el objeto de la clase externa creamos el de la interna
+        System.out.println(c_interna.y + c_externa.x); //llamamos los atributos de ambas..
+        */
+        //Si la clase interna tuviera un acceso static....
+        /*
+        Externa.Interna c_interna = new Externa.Interna(); //Solo creamos el objeto con la notacion de punto
+        System.out.println(c_interna.y);
+        */
+        
+        //Acceso a la clase externa desde la interna....
+        Externa c_externa = new Externa(); // creamos un objeto de la clase externa
+        Externa.Interna c_interna = c_externa.new Interna(); //con el objeto de la clase externa creamos el de la interna
+        System.out.println(c_interna.metodoInterno());
     }
     
 }
