251fface54a752acdd642edd502aba3072c4bb29
andre <andre@>
2021-07-13 21:03:00 -0500

    prog orie obj pruebas Temas herencia abstractas interfaces enum

---
+++ src/main/java/misiontic/utp/grupo09/sem_03/progorienobj/FrecAlim.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package misiontic.utp.grupo09.sem_03.progorienobj;
+
+/**
+ *
+ * @author andre
+ */
+interface FrecAlim {
+    public void frec_am();
+    public void frec_pm();
+    
+}
