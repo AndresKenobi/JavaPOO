70a35c1ec730b1187f65ee9913e996f180464d82
andre <andre@>
2021-07-13 21:02:15 -0500

    prog orie obj pruebas Temas herencia abstractas interfaces enum

---
+++ src/main/java/misiontic/utp/grupo09/sem_03/progorienobj/Pig.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package misiontic.utp.grupo09.sem_03.progorienobj;
+
+/**
+ *
+ * @author andre
+ */
+public class Pig extends Animal {
+
+    @Override
+    public void animalSound() {
+        System.out.println("The pig says: oink oink"); //Es la subclase la que le da el cuerpo a la funcion/metodo.
+    }
+    
+}
